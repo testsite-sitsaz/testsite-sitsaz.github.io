d4e2e9d7b4469af19cfc4ee8c0b3151e9c74d5b6
testsite-sitsaz <ahooramaleki404.204@gmail.com>
2026-09-05 20:59:59 +0330

    Delete get_images.php

--- get_images.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$images = glob("image/*.{jpg,jpeg,png,gif,webp,avif,bmp,svg,JPG,JPEG,PNG,GIF,WEBP,AVIF,BMP,SVG}", GLOB_BRACE);
-echo json_encode($images);
-?>
